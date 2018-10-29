88b42763c1be5fb88d6fdffa49f7a5cb8cd7d47a
Anshu Raj <imanshuraj@gmail.com>
2018-10-29 17:42:56 +0530

    Login user API and send JWT in response

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 // Load User model
 const User = require('../../model/User');
@@ -10,7 +11,7 @@ const User = require('../../model/User');
 // @access  Public
 router.get('/test', (req, res) => res.json({msg: 'User works!'}));
 
-// @route   POST
+// @route   POST api/users/register
 // @desc    Register user
 // @access  Public
 router.post('/register', (req, res) => {
@@ -36,6 +37,46 @@ router.post('/register', (req, res) => {
           })
         });
       }
+    })
+});
+
+// @route   POST api/users/login
+// @desc    Login user / Return JWT
+// @access  Public
+router.post('/login', (req, res) => {
+  const {email, password} = req.body;
+  // const password = req.body.password;
+
+  // Find user by email
+  User.findOne({email})
+    .then(user => {
+      //Check for user
+      if(!user) {
+        return res.status(404).json({email: 'User not found'});
+      }
+
+      // Check password
+      bcrypt.compare(password, user.password)
+        .then(isMatch => {
+          if(isMatch) {
+            const payload = {
+              id: user.id,
+              name: user.name,
+              email: user.email
+            };
+
+            // Generate JWT
+            jwt.sign(
+              payload,
+              'secret',
+              { expiresIn: 3600 },
+              (err, token) => {
+                res.json({success: true, token: `Bearer ${token}`});
+            });
+          } else {
+            return res.status(400).json({password: 'Password incorrect'});
+          }
+        });
     })
 });
 
